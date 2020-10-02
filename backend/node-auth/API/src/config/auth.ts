15665249fefbfcfd341ea2670246a0187968564f
Thdepas <thdepas@gmail.coml.com>
2020-10-02 16:24:03 +0200

    clean

--- backend/node-auth/API/src/config/auth.ts
+++
@@ -1,2 +0,0 @@
-export const BCRYPT_WORK_FACTOR = 12;
-export const BCRYPT_MAX_BYTES = 72;
--- backend/node-auth/API/src/config/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./auth";
-export * from "./db";
-export * from "./app";
-export * from "./cache";
-export * from "./session";
